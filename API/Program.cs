6c9241ba26f9400beff68cda4abce860a514f4cb
ido <idogross1@gmail.com>
2023-02-12 22:33:04 +0200

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<DataContext>(opt => {
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'Dating app';
+  users: any;
+  
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:7000/api/users').subscribe({
+      next: (response) => this.users = response,
+      error: err => console.log(err),
+      complete: () => console.log('done')
+    })
+  }
+
+}
